4898a55c7d4e74e8d72bcc05c038b2e723c9274c
Tomonori-Tanaka <tanaka.t.bj@gmail.com>
2023-01-27 14:30:18 +0900

    mkdir_mld.py

--- mkdir_lmd.py
+++ mkdir_lmd.py
@@ -4,6 +4,7 @@ import re
 import shutil
 import sys
 from numpy import linspace
+import subprocess
 
 ANGSTROM_TO_BOHR = 1.8897261246364832
 REPLACED_KEYWORD_LATTICE_CONST = "AAAAA"
@@ -16,9 +17,10 @@ AFTER_DECIMAL_POINT_ATOMIC_NUM_DIR = 1
 AFTER_DECIMAL_POINT_BOHR = 5
 POTENTIAL_FILE_NAME = "potential.data"
 JOB_SCRIPT_NAME = "job.sh"
-JOB_EXECUTION_COMMAND = f"pjsub {JOB_SCRIPT_NAME}"
+JOB_EXECUTION_COMMAND = f"sbatch {JOB_SCRIPT_NAME}"
 CONVERGENCE_CHECK_KEYWORD = "no convergence"
 OUTPUT_FILE_NAME = "output.dat"
+FMG_FILE_NAME = "fmg.in"
 
 parser = argparse.ArgumentParser(description='Make directory trees: '
                                              'lattice_const/atomic_number/')
@@ -55,29 +57,10 @@ limit_group = parser.add_mutually_exclusive_group()
 help_text = 'subdirectory name; Keyword is restricted to "tc" or "j" in the current version.'
 limit_group.add_argument('-sub', '--subdir_name', choices=['tc', 'j'], help=help_text)
 
-help_text = 'absolute path of root dir when copying potential file; ' \
-            'This option is only available for the make action without subdir_name option.'
-limit_group.add_argument('-path', '--root_path', help=help_text)
-
 help_text = 'calculate only not convergence systems'
 parser.add_argument('--re_calc', action='store_true', help=help_text)
 
-help_text = 'make dlm dir switch and path of fm dir'
-parser.add_argment('-dlm', help=help_text)
-
 args = parser.parse_args()
-
-
-def return_path(*dir_names):
-    """
-    Return path of the directory.
-    :param dir_names: The names of hierarchies
-    :return: path of the directory
-    """
-    path = ""
-    for name in dir_names:
-        path = path + name + "/"
-    return path
 
 
 def replace_input_text(body_text, **replace_keyword):
@@ -91,8 +74,6 @@ def replace_input_text(body_text, **replace_keyword):
         body_text = re.sub(globals()[key], value, body_text)
     return body_text
 
-
-# ----- main part -----
 lattice_constants = linspace(args.lattice_const_start, args.lattice_const_end, args.division_num_lattice_const,
                              endpoint=True)
 atomic_numbers = linspace(args.atomic_number_start, args.atomic_number_end, args.division_num_atomic_num,
@@ -115,7 +96,7 @@ for lattice_const in lattice_constants:
         atomic_num = round(atomic_num, AFTER_DECIMAL_POINT_ATOMIC_NUM_DIR)
         atomic_num_str = "%.*f" % (AFTER_DECIMAL_POINT_ATOMIC_NUM_DIR, atomic_num)
         # get the absolute path of scf directory
-        path_scf = return_path(path_root, lattice_const_str, atomic_num_str)
+        path_scf = os.path.join(path_root, lattice_const_str, atomic_num_str, "lmd")
         path_destination = path_scf
 
         if args.subdir_name:
@@ -123,7 +104,7 @@ for lattice_const in lattice_constants:
                 pass
             else:
                 sys.exit("!ERROR!: Parent directory (SCF directory) does not exist.")
-            path_destination = return_path(path_root, lattice_const_str, atomic_num_str, args.subdir_name)
+            path_destination = os.path.join(path_destination, args.subdir_name)
 
         if args.action == 'make':
             body_replaced = replace_input_text(body_source,
@@ -137,29 +118,25 @@ for lattice_const in lattice_constants:
                 print(e.errno)
                 print(e.filename)
 
-            with open(f'{path_destination}{args.input_file_name}', mode='w', encoding='utf-8') as f:
+            with open(os.path.join(path_destination, args.input_file_name), mode='w', encoding='utf-8') as f:
                 f.write(body_replaced)
             shutil.copy(JOB_SCRIPT_NAME, path_destination)
+            shutil.copy(FMG_FILE_NAME, path_destination)
 
-            if args.root_path:
-                try:
-                    os.path.exists(args.root_path)
-                    path_potential_file_src = return_path(args.root_path, lattice_const_str, atomic_num_str)
-                    shutil.copy(f'{path_potential_file_src}{POTENTIAL_FILE_NAME}', path_destination)
-                except FileExistsError as e:
-                    print(e.strerror)
-                    print(e.errno)
-                    print(e.filename)
+            # make lmd potential data
+            if not args.subdir_name:
+                os.chdir(path_destination)
+                subprocess.run("fmg < fmg.in", shell=True)
+                os.chdir(path_destination)
+
 
             if args.subdir_name:
                 try:
-                    shutil.copy(f'{path_scf}{POTENTIAL_FILE_NAME}', path_destination)
+                    shutil.copy(os.path.join(path_scf, POTENTIAL_FILE_NAME), path_destination)
                 except:
                     print("WARNING: Probably potential file data does not exist.")
 
-
         elif args.action == 'job':
-            import subprocess
 
             os.chdir(path_destination)
             if args.re_calc:
